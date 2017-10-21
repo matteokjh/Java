3940c0afd981a22982f04e5a25bee3ef84fe4876
matteokjh <429797371@qq.com>
2017-10-22 01:25:28 +0800

    最终

---
+++ LibraryManageSystem/src/if_empty_code/siginin.java
@@ -0,0 +1,112 @@
+/*
+ * Created by JFormDesigner on Fri Oct 20 16:51:15 GMT+08:00 2017
+ */
+
+package if_empty_code;
+
+import java.awt.*;
+import javax.swing.*;
+import net.miginfocom.swing.*;
+
+/**
+ * @author matteo matteo
+ */
+public class siginin extends JFrame {
+    public siginin() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+        // Generated using JFormDesigner Evaluation license - matteo matteo
+        dialogPane = new JPanel();
+        contentPanel = new JPanel();
+        label2 = new JLabel();
+        label1 = new JLabel();
+        buttonBar = new JPanel();
+        okButton = new JButton();
+
+        //======== this ========
+        Container contentPane = getContentPane();
+        contentPane.setLayout(new BorderLayout());
+
+        //======== dialogPane ========
+        {
+
+            // JFormDesigner evaluation mark
+            dialogPane.setBorder(new javax.swing.border.CompoundBorder(
+                new javax.swing.border.TitledBorder(new javax.swing.border.EmptyBorder(0, 0, 0, 0),
+                    "JFormDesigner Evaluation", javax.swing.border.TitledBorder.CENTER,
+                    javax.swing.border.TitledBorder.BOTTOM, new java.awt.Font("Dialog", java.awt.Font.BOLD, 12),
+                    java.awt.Color.red), dialogPane.getBorder())); dialogPane.addPropertyChangeListener(new java.beans.PropertyChangeListener(){public void propertyChange(java.beans.PropertyChangeEvent e){if("border".equals(e.getPropertyName()))throw new RuntimeException();}});
+
+            dialogPane.setLayout(new BorderLayout());
+
+            //======== contentPanel ========
+            {
+                contentPanel.setLayout(new MigLayout(
+                    "insets dialog,hidemode 3",
+                    // columns
+                    "[fill]" +
+                    "[fill]" +
+                    "[fill]" +
+                    "[fill]" +
+                    "[fill]" +
+                    "[fill]" +
+                    "[fill]" +
+                    "[fill]" +
+                    "[fill]" +
+                    "[fill]",
+                    // rows
+                    "[]" +
+                    "[]" +
+                    "[]" +
+                    "[]" +
+                    "[]"));
+
+                //---- label2 ----
+                label2.setText("text");
+                contentPanel.add(label2, "cell 7 1");
+
+                //---- label1 ----
+                label1.setText("text");
+                contentPanel.add(label1, "cell 9 4");
+            }
+            dialogPane.add(contentPanel, BorderLayout.CENTER);
+
+            //======== buttonBar ========
+            {
+                buttonBar.setLayout(new MigLayout(
+                    "insets dialog,alignx right",
+                    // columns
+                    "[fill]" +
+                    "[fill]" +
+                    "[fill]" +
+                    "[fill]" +
+                    "[fill]" +
+                    "[button,fill]",
+                    // rows
+                    null));
+
+                //---- okButton ----
+                okButton.setText("OK");
+                buttonBar.add(okButton, "cell 1 0");
+            }
+            dialogPane.add(buttonBar, BorderLayout.SOUTH);
+        }
+        contentPane.add(dialogPane, BorderLayout.CENTER);
+        pack();
+        setLocationRelativeTo(getOwner());
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+    // Generated using JFormDesigner Evaluation license - matteo matteo
+    private JPanel dialogPane;
+    private JPanel contentPanel;
+    private JLabel label2;
+    private JLabel label1;
+    private JPanel buttonBar;
+    private JButton okButton;
+    // JFormDesigner - End of variables declaration  //GEN-END:variables
+}
